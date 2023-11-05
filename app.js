2e52b5bc70273d54bc0497e486606932b6b5ab84
edpomacedo <edpomacedo@gmail.com>
2023-11-04 23:55:00 -0300

    cache

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 // Rota para a página inicial
 app.get('/', (req, res) => {
     // Define o controle de cache
-    res.setHeader('Cache-Control', 'public, s-maxage=1');
+    res.setHeader('Cache-Control', 'public, max-age=3600');
     // Envia o arquivo HTML da página inicial
     res.sendFile(__dirname + '/public/index.html');
 });
